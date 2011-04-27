f6a5a370f9e15f3c8d89286b4083e98e46e38f47
hotwatermorning <hotwatermorning@gmail.com>
2011-04-27 17:42:39 +0900

    added floating point value supports for div/mod.

--- trunk/hwm/arithmetic.hpp
+++ trunk/hwm/arithmetic.hpp
@@ -6,15 +6,23 @@
 #ifndef HWM_ARITHMETIC_HPP
 #define HWM_ARITHMETIC_HPP
 
+//! hwm.Arithmetic
+//! abs, sign, modulus, division
+//! @file
+
 #include <cmath>
 #include <boost/assert.hpp>
 #include <boost/mpl/and.hpp>
-#include <boost/type_traits.hpp>
+#include <boost/static_assert.hpp>
+#include <boost/type_traits/is_floating_point.hpp>
+#include <boost/type_traits/is_integral.hpp>
+#include <boost/type_traits/is_signed.hpp>
+#include <boost/type_traits/is_unsigned.hpp>
 #include <boost/utility/enable_if.hpp>
 
-namespace hwm {
-namespace arithmetic {
+namespace hwm { namespace arithmetic {
 
+	//! @cond DETAIL
 	namespace detail
 	{
 		template<typename T>
@@ -51,83 +59,247 @@ namespace arithmetic {
 			return ::fabs(t);
 		}
 	}	//namespace detail
-
+	//! @endcond
+	
+	//! @brief round simply
+	//! @return floor(t + 0.5) casted as type of t.
 	template<typename T>
-	double simple_round(T const &t) { return floor((double)t + 0.5); }
-
-	template<typename T>
-	bool	odd(T const &t) { return t % 2 != 0; }
-
-	template<typename T>
-	bool	even(T const &t) { return t % 2 == 0; }
-
-	template<typename T>
-	int	sign	(T const &t) { return ((t>0)*1)+((t<0)*(-1)); }
-
-	template<typename T>
-	T abs(T const &t) { return detail::abs_switch(t); }
-
-	template<typename T>
-	T	mod_truncated(T const &dividend, T const &divisor)
+	T	round_simple(T f)
 	{
-		BOOST_ASSERT(divisor != 0);
-		return (detail::abs_switch(dividend) % detail::abs_switch(divisor)) * sign(dividend);
+		return static_cast<T>(floor(static_cast<double>(f) + 0.5));
 	}
 
+	//!	@brief round to nearest even.
+	//! also known as banker's round.
+	//! @return floor(t + 0.5) but when reminder is just 0.5, the return will be rounded to nearest even.
+	//! and the return is casted as type of t.
 	template<typename T>
-	T	mod_floored(T const &dividend, T const &divisor)
+	T		round_to_nearest_even	(T f)
 	{
-		BOOST_ASSERT(divisor != 0);
-		int const	dividend_sign		= sign(dividend);
-		bool const 	positive_dividend	= dividend >= 0;
-		bool const	positive_divisor	= divisor >= 0;
-
-		T const unsigned_modulo = (detail::abs_switch(dividend)) % (detail::abs_switch(divisor));
-		return
-			unsigned_modulo * dividend_sign +
-			((int)((unsigned_modulo != 0) && (positive_dividend==false)) * detail::abs_switch(divisor)) -
-			((int)((unsigned_modulo != 0) && (positive_divisor==false)) * detail::abs_switch(divisor));
+		double int_part;
+		double const frac_part = modf(f, &int_part);
+		if		( frac_part < 0.5 )		{ return static_cast<T>(int_part); }
+		else if	( frac_part == 0.5 )	{ return static_cast<T>(int_part + fmod(int_part, 2.0)); }
+		else							{ return static_cast<T>(int_part + 1.0); }
 	}
 
+	//! @return if `t' is odd.
+	//! the behaviour is unspecified if operator % is overloaded.
 	template<typename T>
-	T	mod_euclidean(T const &dividend, T const &divisor)
-	{
-		BOOST_ASSERT(divisor != 0);
-		const int	dividend_sign		= sign(dividend);
-		const bool 	positive_dividend	= dividend >= 0;
+	bool	odd						(T const &t) { return t % 2 != 0; }
 
-		T const unsigned_modulo = (detail::abs_switch(dividend)) % (detail::abs_switch(divisor));
-		return
-			unsigned_modulo * dividend_sign +
-			((int)((unsigned_modulo != 0) && (positive_dividend==false)) * detail::abs_switch(divisor));
-	}
-
+	//! @return if `t' is even.
+	//! the behaviour is unspecified if operator % is overloaded.
 	template<typename T>
-	T	div_truncated(T const &dividend, T const &divisor)
-	{
-		BOOST_ASSERT(divisor != 0);
-		return (detail::abs_switch(dividend) / detail::abs_switch(divisor)) * sign(dividend) * sign(divisor);
-	}
+	bool	even					(T const &t) { return t % 2 == 0; }
 
+	//! @brief return the signature as 1, 0 or -1
+	//! @return return 1 if `t' is positive, -1 if `t' is negative, and 0 if `t' is zero.
 	template<typename T>
-	T	div_floored(T const &dividend, T const &divisor)
+	int		sign					(T const &t) { return ((t>0)*1)+((t<0)*(-1)); }
+
+	//! @return return abs(t) if `t' is integral, return fabs(t) if `t' is floating point number.
+	template<typename T>
+	T		abs						(T const &t) { return detail::abs_switch(t); }
+
+	//============================================================================//
+	//! @defgroup modulus Modulus Operations.
+	//!	<a href ="http://en.wikipedia.org/wiki/Modulo_operation">see also</a>
+	//! @{
+	//============================================================================//
+	
+	//fmod(x, y) : x = i * y + f, return f;
+	//sign(f) == sign(x), abs(f) < abs(y)
+
+	//! @brief modulus operation for integral value.
+	//! @return return modulo by truncated division.
+	template<typename T>
+	T		mod_truncated			(T const &dividend, T const &divisor)
 	{
 		BOOST_ASSERT(divisor != 0);
 		return
-			(detail::abs_switch(dividend) / detail::abs_switch(divisor)) * sign(dividend) * sign(divisor) -
-			(int)(dividend % divisor != 0) * ((dividend >= 0) ^ (divisor >= 0));
+			(detail::abs_switch(dividend) % detail::abs_switch(divisor)) *
+			sign(dividend);
 	}
 
+	//! @brief modulus operation for floating point value.
+	//! @return return modulo by truncated division.
+	inline
+	double	fmod_truncated			(double dividend, double divisor)
+	{
+		BOOST_ASSERT(divisor != 0);
+		return fmod(dividend, divisor);
+	}
+
+	//! @brief modulus operation for integral value.
+	//! @return return modulo by floored division.
 	template<typename T>
-	T	div_euclidean(T const &dividend, T const &divisor)
+	T		mod_floored				(T const &dividend, T const &divisor)
+	{
+		BOOST_ASSERT(divisor != 0);
+
+		bool const is_dividend_negative	= dividend < 0;
+		bool const is_divisor_negative	= divisor < 0;
+		bool const exclusive_sign		= is_dividend_negative ^ is_divisor_negative;
+		
+		T const mod_tr = mod_truncated(dividend, divisor);
+
+		return
+			mod_tr +
+			(exclusive_sign * (mod_tr != 0) * divisor);
+	}
+
+	//! @brief modulus operation for floating point value.
+	//! @return return modulo by floored division.
+	inline
+	double	fmod_floored			(double dividend, double divisor)
+	{	
+		BOOST_ASSERT(divisor != 0);
+
+		bool const is_dividend_negative	= dividend < 0;
+		bool const is_divisor_negative	= divisor < 0;
+		bool const exclusive_sign		= is_dividend_negative ^ is_divisor_negative;
+
+		double const mod_tr = fmod_truncated(dividend, divisor);
+
+		return
+			mod_tr +
+			(exclusive_sign * (mod_tr != 0) * divisor);
+	}
+
+	//! @brief modulus operation for integral value.
+	//! @return return modulo by Euclidean division.
+	template<typename T>
+	T		mod_euclidean			(T const &dividend, T const &divisor)
+	{
+		BOOST_ASSERT(divisor != 0);
+
+		bool const	is_dividend_negative	= dividend < 0;
+		T const mod_tr = mod_truncated(dividend, divisor);
+
+		return
+			mod_tr +
+			(is_dividend_negative * (mod_tr != 0) * detail::abs_switch(divisor));
+	}
+
+	//! @brief modulus operation for floating point value.
+	//! @return return modulo by Euclidean division.
+	inline
+	double	fmod_euclidean			(double dividend, double divisor)
+	{
+		BOOST_ASSERT(divisor != 0);
+		bool const	is_dividend_negative	= dividend < 0;
+
+		double const mod_tr = fmod_truncated(dividend, divisor);
+
+		return
+			mod_tr +
+			(is_dividend_negative * (mod_tr != 0) * detail::abs_switch(divisor));
+	}
+
+	//============================================================================//
+	//! @}
+	//	enddef of modulus
+	//============================================================================//
+
+	//============================================================================//
+	//! @defgroup division Division Operations.
+	//!	<a href="http://en.wikipedia.org/wiki/Modulo_operation">see also</a>
+	//! @{
+	//============================================================================//
+
+	//! @brief division operation for integral value.
+	//! @return return quatient by truncated division.
+	template<typename T>
+	T		div_truncated			(T const &dividend, T const &divisor)
 	{
 		BOOST_ASSERT(divisor != 0);
 		return
-			(detail::abs_switch(dividend) / detail::abs_switch(divisor)) * sign(dividend) * sign(divisor) -
-			(int)(dividend % divisor != 0) * (((dividend >= 0) ^ (divisor >= 0)) - (int)(divisor < 0));
+			(detail::abs_switch(dividend) / detail::abs_switch(divisor)) *
+			sign(dividend) *
+			sign(divisor);
 	}
 
-}	//namespace arithmetic
-}	//namespace hwm
+	//! @brief division operation for floating point value.
+	//! @return return quatient by truncated division.
+	inline
+	double	fdiv_truncated			(double dividend, double divisor)
+	{
+		BOOST_ASSERT(divisor != 0);
+	
+		bool const is_divisor_negative	= divisor < 0;
+
+		double const division = (detail::abs_switch(dividend) / detail::abs_switch(divisor));
+		return
+			floor(division) *
+			sign(dividend) *
+			sign(divisor);
+	}
+
+	//! @brief division operation for integral value.
+	//! @return return quatient by floored division.
+	template<typename T>
+	T		div_floored				(T const &dividend, T const &divisor)
+	{
+		BOOST_ASSERT(divisor != 0);
+		bool const is_dividend_negative	= dividend < 0;
+		bool const is_divisor_negative	= divisor < 0;
+		bool const exclusive_sign		= is_dividend_negative ^ is_divisor_negative;
+
+		double const mod_tr = fmod_truncated(dividend, divisor);
+		return
+			div_truncated(dividend, divisor) +
+			((int)exclusive_sign * (int)(mod_tr != 0) * (-1));
+	}
+
+	//! @brief division operation for floating point value.
+	//! @return return quatient by floored division.
+	inline
+	double	fdiv_floored			(double dividend, double divisor)
+	{
+		BOOST_ASSERT(divisor != 0);
+		bool const is_dividend_negative	= dividend < 0;
+		bool const is_divisor_negative	= divisor < 0;
+		bool const exclusive_sign		= is_dividend_negative ^ is_divisor_negative;
+
+		double const mod_tr = fmod_truncated(dividend, divisor);
+		return
+			fdiv_truncated(dividend, divisor) +
+			((int)exclusive_sign * (int)(mod_tr != 0) * (-1));
+	}
+
+	//! @brief division operation for integral value.
+	//! @return return quatient by Euclidean division.
+	template<typename T>
+	T		div_euclidean			(T const &dividend, T const &divisor)
+	{
+		BOOST_ASSERT(divisor != 0);
+		bool const is_divisor_negative	= divisor < 0;
+
+		double const mod_tr = fmod_truncated(dividend, divisor);
+		return 
+			div_floored(dividend, divisor) +
+			((int)is_divisor_negative * (int)(mod_tr != 0) * 1);
+	}
+
+	//! @brief division operation for floating point value.
+	//! @return return quatient by Euclidean division.
+	inline
+	double	fdiv_euclidean			(double dividend, double divisor)
+	{
+		BOOST_ASSERT(divisor != 0);
+		bool const is_divisor_negative	= divisor < 0;
+
+		double const mod_tr = fmod_truncated(dividend, divisor);
+		return 
+			fdiv_floored(dividend, divisor) +
+			((int)is_divisor_negative * (int)(mod_tr != 0) * 1);
+	}
+	//============================================================================//
+	//!	@}
+	// enddef of division
+	//============================================================================//
+
+}}	//namespace hwm::arithmetic
 
 #endif	//HWM_ARITHMETIC_HPP
--- trunk/libs/arithmetic.cpp
+++ trunk/libs/arithmetic.cpp
@@ -6,15 +6,25 @@
 #include <boost/test/minimal.hpp>
 #include "../hwm/arithmetic.hpp"
 
-int test_main(int, char **)
+int test_main(int, char**)
 {
 	namespace har = hwm::arithmetic;
-	BOOST_CHECK(har::simple_round(0.00) == 0.0);
-	BOOST_CHECK(har::simple_round(0.49) == 0.0);
-	BOOST_CHECK(har::simple_round(0.50) == 1.0);
-	BOOST_CHECK(har::simple_round(0.51) == 1.0);
-	BOOST_CHECK(har::simple_round(1.00) == 1.0);
-	BOOST_CHECK(har::simple_round(1.50) == 2.0);
+	BOOST_CHECK(har::round_simple(0.00) == 0.0);
+	BOOST_CHECK(har::round_simple(0.49) == 0.0);
+	BOOST_CHECK(har::round_simple(0.50) == 1.0);
+	BOOST_CHECK(har::round_simple(0.51) == 1.0);
+	BOOST_CHECK(har::round_simple(1.00) == 1.0);
+	BOOST_CHECK(har::round_simple(1.50) == 2.0);
+
+	BOOST_CHECK(har::round_to_nearest_even(0.00) == 0.0);
+	BOOST_CHECK(har::round_to_nearest_even(0.49) == 0.0);
+	BOOST_CHECK(har::round_to_nearest_even(0.50) == 0.0);
+	BOOST_CHECK(har::round_to_nearest_even(0.51) == 1.0);
+	BOOST_CHECK(har::round_to_nearest_even(1.00) == 1.0);
+	BOOST_CHECK(har::round_to_nearest_even(1.50) == 2.0);
+	BOOST_CHECK(har::round_to_nearest_even(2.50) == 2.0);
+	BOOST_CHECK(har::round_to_nearest_even(3.50) == 4.0);
+	BOOST_CHECK(har::round_to_nearest_even(4.50) == 4.0);
 
 	BOOST_CHECK(har::odd(3) == true);
 	BOOST_CHECK(har::even(3) == false);
@@ -25,6 +35,9 @@ int test_main(int, char **)
 	unsigned int	const un[] = { -5, 0, 5 };
 	double			const fn[] = { -5, 0, 5 };
 
+	//============================================================================//
+	//	integral
+	//============================================================================//
 	BOOST_CHECK(har::sign(in[0]) == -1);
 	BOOST_CHECK(har::sign(in[1]) == 0);
 	BOOST_CHECK(har::sign(in[2]) == 1);
@@ -45,14 +58,6 @@ int test_main(int, char **)
 	BOOST_CHECK(har::abs(fn[1]) == 0);
 	BOOST_CHECK(har::abs(fn[2]) == 5);
 
-	BOOST_CHECK(har::div_euclidean(13, 4) 	== 3);
-	BOOST_CHECK(har::div_euclidean(-13, 4) 	== -4);
-	BOOST_CHECK(har::div_euclidean(13, -4) 	== -3);
-	BOOST_CHECK(har::div_euclidean(-13, -4) == 4);
-	BOOST_CHECK(har::mod_euclidean(13, 4) 	== 1);
-	BOOST_CHECK(har::mod_euclidean(-13, 4) 	== 3);
-	BOOST_CHECK(har::mod_euclidean(13, -4) 	== 1);
-	BOOST_CHECK(har::mod_euclidean(-13, -4) == 3);
 	BOOST_CHECK(har::div_truncated(13, 4) 	== 3);
 	BOOST_CHECK(har::div_truncated(-13, 4) 	== -3);
 	BOOST_CHECK(har::div_truncated(13, -4) 	== -3);
@@ -69,14 +74,95 @@ int test_main(int, char **)
 	BOOST_CHECK(har::mod_floored(-13, 4) 	== 3);
 	BOOST_CHECK(har::mod_floored(13, -4) 	== -3);
 	BOOST_CHECK(har::mod_floored(-13, -4) 	== -1);
+	BOOST_CHECK(har::div_euclidean(13, 4) 	== 3);
+	BOOST_CHECK(har::div_euclidean(-13, 4) 	== -4);
+	BOOST_CHECK(har::div_euclidean(13, -4) 	== -3);
+	BOOST_CHECK(har::div_euclidean(-13, -4) == 4);
+	BOOST_CHECK(har::mod_euclidean(13, 4) 	== 1);
+	BOOST_CHECK(har::mod_euclidean(-13, 4) 	== 3);
+	BOOST_CHECK(har::mod_euclidean(13, -4) 	== 1);
+	BOOST_CHECK(har::mod_euclidean(-13, -4) == 3);
 
+	//============================================================================//
+	//	floating point
+	//============================================================================//
+	BOOST_CHECK(har::fdiv_truncated(13, 4) 	== 3);
+	BOOST_CHECK(har::fdiv_truncated(-13, 4) == -3);
+	BOOST_CHECK(har::fdiv_truncated(13, -4) == -3);
+	BOOST_CHECK(har::fdiv_truncated(-13, -4) == 3);
+	BOOST_CHECK(har::fmod_truncated(13, 4) 	== 1);
+	BOOST_CHECK(har::fmod_truncated(-13, 4) == -1);
+	BOOST_CHECK(har::fmod_truncated(13, -4) == 1);
+	BOOST_CHECK(har::fmod_truncated(-13, -4)== -1);
+	BOOST_CHECK(har::fdiv_floored(13, 4) 	== 3);
+	BOOST_CHECK(har::fdiv_floored(-13, 4) 	== -4);
+	BOOST_CHECK(har::fdiv_floored(13, -4) 	== -4);
+	BOOST_CHECK(har::fdiv_floored(-13, -4) 	== 3);
+	BOOST_CHECK(har::fmod_floored(13, 4) 	== 1);
+	BOOST_CHECK(har::fmod_floored(-13, 4) 	== 3);
+	BOOST_CHECK(har::fmod_floored(13, -4) 	== -3);
+	BOOST_CHECK(har::fmod_floored(-13, -4) 	== -1);
+	BOOST_CHECK(har::fdiv_euclidean(13, 4) 	== 3);
+	BOOST_CHECK(har::fdiv_euclidean(-13, 4) == -4);
+	BOOST_CHECK(har::fdiv_euclidean(13, -4) == -3);
+	BOOST_CHECK(har::fdiv_euclidean(-13, -4)== 4);
+	BOOST_CHECK(har::fmod_euclidean(13, 4) 	== 1);
+	BOOST_CHECK(har::fmod_euclidean(-13, 4) == 3);
+	BOOST_CHECK(har::fmod_euclidean(13, -4) == 1);
+	BOOST_CHECK(har::fmod_euclidean(-13, -4)== 3);
+
+	BOOST_CHECK(har::fdiv_truncated(12, 4) 	== 3);
+	BOOST_CHECK(har::fdiv_truncated(-12, 4) == -3);
+	BOOST_CHECK(har::fdiv_truncated(12, -4) == -3);
+	BOOST_CHECK(har::fdiv_truncated(-12, -4) == 3);
+	BOOST_CHECK(har::fmod_truncated(12, 4) 	== 0);
+	BOOST_CHECK(har::fmod_truncated(-12, 4) == 0);
+	BOOST_CHECK(har::fmod_truncated(12, -4) == 0);
+	BOOST_CHECK(har::fmod_truncated(-12, -4)== 0);
+	BOOST_CHECK(har::fdiv_floored(12, 4) 	== 3);
+	BOOST_CHECK(har::fdiv_floored(-12, 4) 	== -3);
+	BOOST_CHECK(har::fdiv_floored(12, -4) 	== -3);
+	BOOST_CHECK(har::fdiv_floored(-12, -4) 	== 3);
+	BOOST_CHECK(har::fmod_floored(12, 4) 	== 0);
+	BOOST_CHECK(har::fmod_floored(-12, 4) 	== 0);
+	BOOST_CHECK(har::fmod_floored(12, -4) 	== 0);
+	BOOST_CHECK(har::fmod_floored(-12, -4) 	== 0);
+	BOOST_CHECK(har::fdiv_euclidean(12, 4) 	== 3);
+	BOOST_CHECK(har::fdiv_euclidean(-12, 4) == -3);
+	BOOST_CHECK(har::fdiv_euclidean(12, -4) == -3);
+	BOOST_CHECK(har::fdiv_euclidean(-12, -4)== 3);
+	BOOST_CHECK(har::fmod_euclidean(12, 4) 	== 0);
+	BOOST_CHECK(har::fmod_euclidean(-12, 4) == 0);
+	BOOST_CHECK(har::fmod_euclidean(12, -4) == 0);
+	BOOST_CHECK(har::fmod_euclidean(-12, -4)== 0);
+
+	BOOST_CHECK(har::fdiv_truncated(14, 4) 	== 3);
+	BOOST_CHECK(har::fdiv_truncated(-14, 4) == -3);
+	BOOST_CHECK(har::fdiv_truncated(14, -4) == -3);
+	BOOST_CHECK(har::fdiv_truncated(-14, -4) == 3);
+	BOOST_CHECK(har::fmod_truncated(14, 4) 	== 2);
+	BOOST_CHECK(har::fmod_truncated(-14, 4) == -2);
+	BOOST_CHECK(har::fmod_truncated(14, -4) == 2);
+	BOOST_CHECK(har::fmod_truncated(-14, -4)== -2);
+	BOOST_CHECK(har::fdiv_floored(14, 4) 	== 3);
+	BOOST_CHECK(har::fdiv_floored(-14, 4) 	== -4);
+	BOOST_CHECK(har::fdiv_floored(14, -4) 	== -4);
+	BOOST_CHECK(har::fdiv_floored(-14, -4) 	== 3);
+	BOOST_CHECK(har::fmod_floored(14, 4) 	== 2);
+	BOOST_CHECK(har::fmod_floored(-14, 4) 	== 2);
+	BOOST_CHECK(har::fmod_floored(14, -4) 	== -2);
+	BOOST_CHECK(har::fmod_floored(-14, -4) 	== -2);
+	BOOST_CHECK(har::fdiv_euclidean(14, 4) 	== 3);
+	BOOST_CHECK(har::fdiv_euclidean(-14, 4) == -4);
+	BOOST_CHECK(har::fdiv_euclidean(14, -4) == -3);
+	BOOST_CHECK(har::fdiv_euclidean(-14, -4)== 4);
+	BOOST_CHECK(har::fmod_euclidean(14, 4) 	== 2);
+	BOOST_CHECK(har::fmod_euclidean(-14, 4) == 2);
+	BOOST_CHECK(har::fmod_euclidean(14, -4) == 2);
+	BOOST_CHECK(har::fmod_euclidean(-14, -4)== 2);
 
 
 	//work as http://en.wikipedia.org/wiki/Modulo_operation
-	BOOST_CHECK(13 	== har::div_euclidean	(13, 4) 	* 	4 	+ har::mod_euclidean(13, 4)		);
-	BOOST_CHECK(-13 == har::div_euclidean	(-13, 4) 	* 	4 	+ har::mod_euclidean(-13, 4)	);
-	BOOST_CHECK(13 	== har::div_euclidean	(13, -4) 	* 	-4 	+ har::mod_euclidean(13, -4)	);
-	BOOST_CHECK(-13 == har::div_euclidean	(-13, -4) 	* 	-4 	+ har::mod_euclidean(-13, -4)	);
 	BOOST_CHECK(13 	== har::div_truncated	(13, 4) 	* 	4 	+ har::mod_truncated(13, 4)		);
 	BOOST_CHECK(-13 == har::div_truncated	(-13, 4) 	* 	4 	+ har::mod_truncated(-13, 4)	);
 	BOOST_CHECK(13 	== har::div_truncated	(13, -4) 	* 	-4 	+ har::mod_truncated(13, -4)	);
@@ -85,6 +171,10 @@ int test_main(int, char **)
 	BOOST_CHECK(-13 == har::div_floored		(-13, 4) 	* 	4 	+ har::mod_floored	(-13, 4)	);
 	BOOST_CHECK(13 	== har::div_floored		(13, -4) 	* 	-4 	+ har::mod_floored	(13, -4)	);
 	BOOST_CHECK(-13 == har::div_floored		(-13, -4) 	* 	-4 	+ har::mod_floored	(-13, -4)	);
+	BOOST_CHECK(13 	== har::div_euclidean	(13, 4) 	* 	4 	+ har::mod_euclidean(13, 4)		);
+	BOOST_CHECK(-13 == har::div_euclidean	(-13, 4) 	* 	4 	+ har::mod_euclidean(-13, 4)	);
+	BOOST_CHECK(13 	== har::div_euclidean	(13, -4) 	* 	-4 	+ har::mod_euclidean(13, -4)	);
+	BOOST_CHECK(-13 == har::div_euclidean	(-13, -4) 	* 	-4 	+ har::mod_euclidean(-13, -4)	);
 
 	return 0;
 }
